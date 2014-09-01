bc638c34cf98787e833b1ae81b7ae19996ada750
Murali Kota <murali_kota@yahoo.com>
2014-09-01 15:12:11 -0500

    Merge branch 'alpha-version' of https://github.com/CareTiger/HumanLink into alpha-version
    Conflicts:
    templates/landingPageNavbar.html

--- src/profile.py
+++ src/profile.py
@@ -42,6 +42,7 @@ class MainPage(webapp2.RequestHandler):
         else:
              self.redirect(users.create_login_url(self.request.uri))
                              
+                             
 class profileEdit(webapp2.RequestHandler):
     def get(self):       
         user = users.get_current_user()
