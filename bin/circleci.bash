d8ca83cd94aec26e9d7dc0f1d0c1019bc47dd327
Bekt <bekt17@gmail.com>
2014-11-18 15:07:53 -0600

    gsutils on circleci.

--- bin/circleci.bash
+++ bin/circleci.bash
@@ -3,12 +3,12 @@
 # This file contains scripts that are run by CircleCi.
 # Check circle.yml to see how they are being used.
 
+GAE=$HOME/google_appengine/
+GSUTIL=$HOME/gsutil/
+
 # Dependencies pre.
 function dep_pre {
-    # GAE
-    curl -o $HOME/gae.zip https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.15.zip
-    unzip -q -d $HOME $HOME/gae.zip
-
+    echo ">>> bower and gulp install"
     npm install -g bower
     npm install -g gulp
 }
@@ -19,30 +19,54 @@ function dep_post {
     bower install
     gulp compile
 
+}
+
+function gae_deps {
+    # GAE
+    if [ ! -d "$GAE" ]; then
+      echo ">>> Downloading App Engine SDK..."
+      curl -o $HOME/gae.zip https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.15.zip
+      unzip -q -d $HOME $HOME/gae.zip
+    fi
+
+    # Google Cloud Storage
+    if [ ! -d "$GSUTIL" ]; then
+      echo ">>> Downloading gsutil..."
+      curl -o $HOME/gsutil.zip https://storage.googleapis.com/pub/gsutil.zip
+      unzip -q -d $HOME $HOME/gsutil.zip
+    fi
+    # Replacing the oauth token like this because there is
+    # no other easy way.
+    sed "s@\$GS_OAUTH_TOKEN@$GS_OAUTH_TOKEN@" bin/.circleci_boto > $HOME/.boto
+    echo ">>> Downloading configs.py"
+    $GSUTIL/gsutil cp gs://humanlink-private/configs.py .
+
     # pip dependencies
-    echo 'Creating a new virtualenv...'
+    echo '>>> Creating a new virtualenv...'
     virtualenv-2.7 venv
-    echo 'Installing venv libraries'
+    echo '>>> Installing venv libraries'
     ./venv/bin/pip install requests
     ./venv/bin/pip install mandrill
 }
 
 # Deployment.
 function deploy {
+    gae_deps
     app_id=$1
-    $HOME/google_appengine/appcfg.py \
+    $GAE/appcfg.py \
         -v --application=$app_id \
         --oauth2_refresh_token=$GAE_OAUTH_TOKEN \
         update .
-    echo "Deployed to $app_id"
+    echo ">>> Deployed to $app_id"
+    rm -f configs.py
 }
 
 function tests {
-    echo "No tests at the moment."
+    echo ">>> No tests at the moment."
 }
 
 function usage {
-    echo "Unknown option."
+    echo ">>> Unknown option."
     exit 1
 }
 
