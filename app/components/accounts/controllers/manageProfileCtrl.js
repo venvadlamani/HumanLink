1c37d297a9e238407192303ada58f72322d041ad
Mike Nolan <me@michael-nolan.com>
2014-12-26 11:17:38 -0500

    profile uses  for data

--- app/components/accounts/controllers/manageProfileCtrl.js
+++ app/components/accounts/controllers/manageProfileCtrl.js
@@ -6,6 +6,12 @@
 angular
     .module('Accounts')
     .controller('manageProfileCtrl', ['$scope', function ($scope) {
+        $scope.patients = [
+            {name: 'Patient Name', phone: '(555)555-5555'},
+            {name: 'Patient Name', phone: '(555)555-5555'},
+            {name: 'Patient Name', phone: '(555)555-5555'},
+            {name: 'Patient Name', phone: '(555)555-5555'}
+        ];
 
         /**
          * Add's patient under current account.
