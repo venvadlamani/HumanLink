155b13f5dc787127425ec6fb3f611966d99c5abb
Bekt <bekt17@gmail.com>
2014-12-27 19:49:31 -0600

    New and updated API models.

--- models/api/base.py
+++ models/api/base.py
@@ -3,6 +3,24 @@ from google.appengine.ext import ndb
 
 
 class BaseApiModel(EndpointsModel):
-    """Base for all API models."""
+    """Base API model."""
+    # Included fields. If not provided, everything will be included.
+    fields = ndb.StringProperty()
+    # Excluded fields. If not provided, nothing will be excluded.
+    exclude_fields = ndb.StringProperty()
+    # Limit the number of results to this.
+    limit = ndb.IntegerProperty()
+
+
+class SimpleRequest(BaseApiModel):
+    """Simple request model.
+
+    Usually only one of the variables is used with a request.
+    """
+    account_id = ndb.IntegerProperty()
+    patient_id = ndb.IntegerProperty()
+
+
+class SimpleResponse(EndpointsModel):
+    """Simple response model."""
     success_message = ndb.StringProperty()
-    error_message = ndb.StringProperty()
