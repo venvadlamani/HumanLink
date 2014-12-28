6f4d6626c399bd7a0eae800007725773c5bc3e5f
Bekt <bekt17@gmail.com>
2014-12-27 19:50:21 -0600

    Implemented connections API methods.

--- main_api.py
+++ main_api.py
@@ -3,8 +3,10 @@
 import endpoints
 
 from controllers.api.accounts import AccountsApi
+from controllers.api.connections import ConnectionsApi
 
 
 application = endpoints.api_server([
     AccountsApi,
+    ConnectionsApi,
 ], restricted=False)
