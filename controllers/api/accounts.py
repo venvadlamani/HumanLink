45cc897064db52643bb7256597119e1c6038fa79
Bekt <bekt17@gmail.com>
2014-12-27 19:50:48 -0600

    Implemented accounts API methods.

--- controllers/api/accounts.py
+++ controllers/api/accounts.py
@@ -1,41 +1,125 @@
-from controllers.api.base import humanlink_api
-from controllers.api.base import get_current_user
-from controllers.api.base import user_required
-from models.api.accounts import ProfileApiModel
-from services import accounts as account_service
+import services.accounts
+import services.exp as exp
+from controllers.api.base import (
+    handle_exception,
+    humanlink_api,
+    get_current_user,
+    user_required
+)
+from models.api.accounts import (
+    AccountApiModel,
+    CaregiverApiModel,
+    PatientApiModel,
+)
+from models.api.base import (SimpleRequest, SimpleResponse)
 
 from protorpc import remote
 
 
-@humanlink_api.api_class(resource_name='accounts')
+@humanlink_api.api_class(resource_name='accounts', path='accounts')
 class AccountsApi(remote.Service):
 
-    @ProfileApiModel.method(name='profile.create', http_method='POST',
-                            path='accounts/profile/create',
-                            request_fields=('first', 'last', 'phone_number',))
+    @SimpleRequest.method(name='get', path='{account_id}', http_method='GET',
+                          request_fields=('account_id',),
+                          response_message=AccountApiModel.ProtoModel())
     @user_required
-    def create_profile(self, profile_api):
-        """Create a new profile with the given information."""
-        user = get_current_user()
-        profile_dto = ProfileApiModel.to_profile_dto(profile_api)
-        profile_dto = account_service.create_pofile(user.id, profile_dto)
-        return ProfileApiModel.from_profile_dto(profile_dto)
+    def account_get(self, req):
+        """Retrieve the account's profile information.
 
-    @ProfileApiModel.method(name='profile.remove', http_method='POST',
-                            path='accounts/profile/remove',
-                            request_fields=('id',),
-                            response_fields=())
+        :param req.account_id: (int) ID of the account to retrieve.
+        """
+        account_id = req.account_id
+        try:
+            account_dto = services.accounts.account_by_id(account_id)
+            api_model = AccountApiModel.from_account_dto(account_dto)
+            return api_model
+        except exp.ServiceExp as e:
+            handle_exception(e)
+
+    @AccountApiModel.method(name='update', path='update', http_method='POST',
+                            response_message=AccountApiModel.ProtoModel())
     @user_required
-    def remove_profile(self, profile_api):
-        """Remove the given profile."""
-        user = get_current_user()
-        account_service.delete_profile(user.id, profile_api.id)
-        return profile_api
+    def account_update(self, req):
+        """Update account information."""
+        raise NotImplementedError('account.update: not implemented')
 
-    @ProfileApiModel.method(name='profile.get', http_method='GET',
-                            path='accounts/profile/{id}',
-                            request_fields=('id',))
-    def get_profile(self, profile_api):
-        """Get profile details for the given profile."""
-        profile_dto = account_service.profile_by_id(profile_api.id)
-        return ProfileApiModel.from_profile_dto(profile_dto)
+    @SimpleRequest.method(name='caregiver', path='caregiver', http_method='GET',
+                          request_fields=(),
+                          response_message=CaregiverApiModel.ProtoModel())
+    @user_required
+    def caregiver(self, req):
+        """Retrieve the account's caregiver details."""
+        try:
+            account = get_current_user()
+            caregiver_dto = services.accounts.caregiver_by_account(account.id)
+            api_model = CaregiverApiModel.from_caregiver_dto(caregiver_dto)
+            return api_model
+        except exp.ServiceExp as e:
+            handle_exception(e)
+
+    @SimpleRequest.method(name='caregiver.update', path='caregiver/update',
+                          http_method='POST',
+                          response_message=CaregiverApiModel.ProtoModel())
+    @user_required
+    def caregiver_update(self, req):
+        """Update the account's caregiver details."""
+        raise NotImplementedError('caregiver.update: not implemented')
+
+    @SimpleRequest.method(name='patients.list', path='patients/list',
+                          http_method='GET', request_fields=(),
+                          response_message=PatientApiModel.ProtoCollection())
+    @user_required
+    def patients_list(self, req):
+        """Retrieve the account's patients."""
+        try:
+            account = get_current_user()
+            patient_dtos = services.accounts.patients_by_account(account.id)
+            api_models = [PatientApiModel.from_patient_dto(p)
+                          for p in patient_dtos]
+            return PatientApiModel.ProtoCollection()(items=api_models)
+        except exp.ServiceExp as e:
+            handle_exception(e)
+
+    @SimpleRequest.method(name='patients.get', path='patients/{patient_id}',
+                          http_method='GET', request_fields=('patient_id',),
+                          response_message=PatientApiModel.ProtoModel())
+    @user_required
+    def patients_get(self, req):
+        """Retrieve a patient by ID.
+
+        :param req.patient_id: (int) ID of the patient to retrieve.
+        """
+        patient_id = req.patient_id
+        try:
+            account = get_current_user()
+            patient_dto = services.accounts.patient_by_id(account.id, patient_id)
+            api_model = PatientApiModel.from_patient_dto(patient_dto)
+            return api_model
+        except exp.ServiceExp as e:
+            handle_exception(e)
+
+    @PatientApiModel.method(name='patients.update', path='patients/update',
+                            http_method='POST',
+                            response_message=PatientApiModel.ProtoModel())
+    @user_required
+    def patients_update(self, req):
+        """Create or update a single patient."""
+        raise NotImplementedError('patients.update: not implemented')
+
+    @SimpleRequest.method(name='patients.remove', path='patients/remove',
+                          http_method='POST', request_fields=('patient_id',),
+                          response_message=SimpleResponse.ProtoModel())
+    @user_required
+    def patients_remove(self, req):
+        """Remove a single patient.
+
+        :param req.patient_id: (int) ID of the patient to remove.
+        """
+        patient_id = req.patient_id
+        resp = SimpleResponse()
+        try:
+            account_id = get_current_user()
+            services.accounts.patient_remove(account_id, patient_id)
+            return resp.ToMessage()
+        except exp.ServiceExp as e:
+            handle_exception(e)
