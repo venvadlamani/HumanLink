df862fb5db6ee1ddaa86cc650be2e7ce743acf9e
Mike Nolan <me@michael-nolan.com>
2015-01-06 13:09:26 -0500

    added references page

--- app/components/accounts/controllers/settingsReferencesCtrl.js
+++ app/components/accounts/controllers/settingsReferencesCtrl.js
@@ -6,4 +6,24 @@
 angular
     .module('Accounts')
     .controller('settingsReferencesCtrl', ['$scope', function ($scope) {
+        $scope.addReference = function(name, email) {
+            $scope.account.references.push({ name: name, email: email });
+        };
+
+        $scope.account = {
+            references: [
+                {
+                    name: 'Si Robertson',
+                    email: 'sroberston@gmail.com'
+                },
+                {
+                    name: 'Wayne Hoyt',
+                    email: 'whoyt@gmail.com'
+                },
+                {
+                    name: 'Reynold Grover',
+                    email: 'rgrover@gmail.com'
+                }
+            ]
+        };
     }]);
