a423e6e09643f077c5f86c853371bd2112d357ea
venkatesh <venkatesh.caretiger@gmail.com>
2015-01-06 14:06:44 -0600

    updating the landing page

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,6 @@ gulp.task('move-bootstrap-dependencies', function () {
 gulp.task('compile-less', ['move-bootstrap-dependencies'], function () {
     return gulp.src([
         'assets/stylesheets/less/humanlink.less',
-        'bower_components/pagepiling.js/jquery.pagepiling.css',
     ])
         .pipe(sourcemaps.init())
         .pipe(less())
@@ -39,7 +38,6 @@ gulp.task('compile-js', function() {
         'bower_components/angular/angular.js',
         'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
         'bower_components/angular-ui-router/release/angular-ui-router.js',
-        'bower_components/pagepiling.js/jquery.pagepiling.min.js',
         'app/app.js',
         'app/components/*/*.js',
         'app/components/*/*/*.js',
