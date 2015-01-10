0f6be15da9f21899302a88948f224e8830765640
venkatesh <venkatesh.caretiger@gmail.com>
2015-01-10 16:46:46 -0600

    Making changes to connections and fvaorites sections

--- app/components/connections/controllers/connectionsBaseCtrl.js
+++ app/components/connections/controllers/connectionsBaseCtrl.js
@@ -5,50 +5,21 @@
  */
 angular
     .module('Connections')
-    .controller('connectionsBaseCtrl', ['$scope', '$window', function ($scope, $window) {
-    	
+    .controller('connectionsBaseCtrl', ['$scope', 'apiService', 'userSession',
+    function ($scope, apiService, userSession) {
+
         /**
          * Populates $scope with connections.
          *
          * @returns void
          */
-    	var getConnections = function() {
-            console.log('getConnections');               
-            //name-value pairs
-            return [
-                  {
-               	   'connectionId': '001',
-            	   'firstName': 'Jane',
-            	   'lastName': 'Q. Prototype',
-            	   'imageURL': 'images/victoria-vorel.jpg',
-            	   'role': 'Caregiver',
-            	   'caregiverRating': '3',
-            	   'tagFavorites': true},
-            	  {
-            	   'connectionId': '002',
-				   'firstName': 'Juliet',
-				   'lastName': 'P. Public',
-				   'imageURL': 'images/ashley-turner.jpg',
-				   'role': 'senior transitions manager',
-            	   'caregiverRating': null,
-				   'tagFavorites': false},
-            	  {
-	               'connectionId': '003',
-        		   'firstName': 'Juliet 4',
-        		   'lastName': 'P. Public',
-        		   'imageURL': 'images/ashley-turner.jpg',
-        		   'role': 'senior transitions manager',
-            	   'caregiverRating': null,
-        		   'tagFavorites': false},
-             	  {
-                   'connectionId': '004',
-        		   'firstName': 'Juliet 5',
-        		   'lastName': 'P. Public',
-        		   'imageURL': 'images/ashley-turner.jpg',
-        		   'role': 'senior transitions manager',
-            	   'caregiverRating': null,
-        		   'tagFavorites': false}        		   
-        		   ];            
-    	 };        
-    	$scope.connections = getConnections();    		    		
+        var connectionsHelper = new HL.CtrlHelper();
+        function getConnectionsInfo() {
+            connectionsHelper.success = function (data, status, headers, config) {
+                $scope.connections = data.items;
+            };
+        apiService.Connections.my({}, connectionsHelper);
+        }
+        getConnectionsInfo();
+
     }]);
