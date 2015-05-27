edd7f9dea5fceda887921e7f37434f514a8da18a
Bekt <bekt17@gmail.com>
2015-05-27 17:18:38 -0500

    map_props documentation.

--- models/dto/__init__.py
+++ models/dto/__init__.py
@@ -1,22 +1,23 @@
 def map_props(target, source, properties):
-    """Maps the given property values of target object to source object.
+    """Maps the given property values of source object to target object.
+    If the target object already has the property, that property is skipped.
 
     Example:
         given:
-            source = A,
-            target = B (B.hi == '11', B.hello == '22'),
-            properties = ['hi']
+            target: A (A.hello == '00')
+            source: B (B.hi == '11', B.hello == '22', B.foo == '33'),
+            properties: ['hi', 'hello']
         after the function:
-            A: A.hi == '11'
+            A: A.hi == '11', A.hello == '00'
             B: not changed
 
-    :param source: an instance of target object.
-    :param target: an instance of source object.
+    :param target: an instance of the target object.
+    :param source: an instance of the source object.
     :param properties: list of properties to map
     :return: (None)
     """
     for k in properties:
-        if not hasattr(source, k):
+        if not hasattr(source, k) or getattr(target, k, None) is not None:
             continue
         v = getattr(source, k)
         setattr(target, k, v)
