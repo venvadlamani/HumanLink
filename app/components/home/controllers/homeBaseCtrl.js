26a8f5b35de6b87997868f76fbd39459006caff1
venkatesh <venkatesh.caretiger@gmail.com>
2016-02-14 09:46:55 -0600

    Added search controller and updated the views.

--- app/components/home/controllers/homeBaseCtrl.js
+++ app/components/home/controllers/homeBaseCtrl.js
@@ -10,12 +10,27 @@ angular
         // CtrlHelper that is shared between the parent and all children.
         $scope.ctrlHelper = new HL.CtrlHelper();
 
+        $scope.searchResults = [
+            {
+                'name': 'Ven Vadlamani',
+                'phone': '(773) 844 - 7312'
+            },
+            {
+                'name': 'Smita Vadlamani',
+                'phone': '(773) 844 - 7311'
+            },
+        ];
+
         /**
          * Go back to the previous page/view.
          * @return void
          */
         $scope.previous = function () {
             $window.history.back();
+        };
+
+        $scope.find = function (model) {
+
         };
 
     }]);
