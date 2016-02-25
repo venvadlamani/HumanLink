9b56b12ab8b195425ed9771e08e9eee52317b94b
venkatesh <venkatesh.caretiger@gmail.com>
2016-02-25 15:16:12 -0600

    Cleaned up the UI for most views.

--- app1/common/common.module.js
+++
@@ -1,9 +0,0 @@
-/**
- * A module that has common directives, services, constants, etc.
- */
-(function () {
-    'use strict';
-
-    angular.module('app.common', []);
-
-})();
--- app1/repo/repo.module.js
+++
@@ -1,9 +0,0 @@
-/**
- * Repository module that communicates with the backend APIs.
- */
-(function () {
-    'use strict';
-
-    angular.module('app.repo', []);
-
-})();
